738515e9197438d74ddee722f9f14dd5e57c5a2e
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-05-09 15:03:14 +0530

    Added comments for Patient repository

--- patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
+++ patient-service/src/main/java/com/pm/patientservice/repository/PatientRepository.java
@@ -7,5 +7,14 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface PatientRepository extends JpaRepository<Patient, UUID> {
+public interface PatientRepository extends JpaRepository<Patient, UUID>
+// Here we pass:
+// 1. Patient -> the Entity class that JPA will manage
+// 2. UUID -> the datatype of the @Id field in the Patient entity i.e., primary key type of our entity class 'Patient'
+// By extending JpaRepository, Spring Data JPA automatically provides
+// CRUD operations and database interaction methods for the Patient entity,
+// So JpaRepository<Patient, UUID> means:
+//=>"Create repository operations for Patient entity whose primary key type is UUID."
+{
+
 }
