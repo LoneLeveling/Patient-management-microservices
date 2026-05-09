87aa4d158b2759364f37d0e9cde0c4ce7d12ff81
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-05-09 14:43:08 +0530

    Commits for Patient-service

---
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -0,0 +1,30 @@
+package com.pm.patientservice.service;
+
+import com.pm.patientservice.dto.PatientResponseDTO;
+import com.pm.patientservice.mapper.PatientMapper;
+import com.pm.patientservice.model.Patient;
+import com.pm.patientservice.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PatientService {
+private PatientRepository patientRepository;
+// Spring injects PatientRepository dependency through constructor Injection
+public PatientService(PatientRepository patientRepository)
+{
+    this.patientRepository=patientRepository;
+}
+
+public List<PatientResponseDTO> getPatients()
+{
+
+    List<Patient> patients=patientRepository.findAll();
+    List<PatientResponseDTO> patientResponseDTOS=
+            patients.stream().map(patient -> PatientMapper.toDTO(patient))
+                    .toList();
+
+    return patientResponseDTOS;
+}
+}
