f2b531326c6c8955a300e3c62a74131607d66c97
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-05-09 17:12:32 +0530

    Added comments for Service class

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -7,16 +7,19 @@ import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+//Service Layer: Where all the business logic + DTO conversion happens for a given request.
 @Service
 public class PatientService {
 private PatientRepository patientRepository;
 // Spring injects PatientRepository dependency through constructor Injection
 public PatientService(PatientRepository patientRepository)
 {
+
     this.patientRepository=patientRepository;
 }
 
+//NOTE: Service layer converts our domain entity model into Response DTO
+    //And the patient response DTO has the properties that we want the client to see.
 public List<PatientResponseDTO> getPatients()
 {
 
