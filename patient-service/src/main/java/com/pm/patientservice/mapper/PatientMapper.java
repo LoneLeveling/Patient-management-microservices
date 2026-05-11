9a05e594737b3b8b20b6f93c2d41f4a63a35687b
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-05-11 14:36:38 +0530

    added comments to Mapper

--- patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
+++ patient-service/src/main/java/com/pm/patientservice/mapper/PatientMapper.java
@@ -2,19 +2,24 @@ package com.pm.patientservice.mapper;
 
 import com.pm.patientservice.dto.PatientResponseDTO;
 import com.pm.patientservice.model.Patient;
-
+//We are using this separate 'Mapper' class which is basically a Helper class,
+// used to convert from Entity to DTO,
+//to keep things more modular & it keeps all the logic out of service layer
 public class PatientMapper
 {
+
+    //PatientResponseDTO :: Entity--> ResponseDTO
     public static PatientResponseDTO toDTO(Patient patient)
             //In above argument we have patient entity class which we want
     // to convert to Patient DTO response.
     {
-        PatientResponseDTO patientResponseDTO=new PatientResponseDTO();
-        patientResponseDTO.setId(patient.getId().toString());//here we are fetching Id from patient entity and have set it as being the ID on our Patient DTO.
-        patientResponseDTO.setName(patient.getName());
-        patientResponseDTO.setAddress(patient.getAddress());
-        patientResponseDTO.setEmail(patient.getEmail());
-        patientResponseDTO.setDateOfBirth(patient.getDateOfBirth().toString());
-        return patientResponseDTO;
+        PatientResponseDTO dto=new PatientResponseDTO();
+        dto.setId(patient.getId().toString());//here we are fetching Id from
+        // patient entity and have set it as being the ID on our Patient DTO.
+        dto.setName(patient.getName());
+        dto.setAddress(patient.getAddress());
+        dto.setEmail(patient.getEmail());
+        dto.setDateOfBirth(patient.getDateOfBirth().toString());
+        return dto;
     }
 }
