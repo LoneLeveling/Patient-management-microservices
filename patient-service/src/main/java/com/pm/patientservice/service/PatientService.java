2200d8b713fb24783f82e24f324276f84d8f9688
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-05-11 12:55:12 +0530

    PatientService updated

--- patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/pm/patientservice/service/PatientService.java
@@ -5,29 +5,26 @@ import com.pm.patientservice.mapper.PatientMapper;
 import com.pm.patientservice.model.Patient;
 import com.pm.patientservice.repository.PatientRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 //Service Layer: Where all the business logic + DTO conversion happens for a given request.
 @Service
-public class PatientService {
-private PatientRepository patientRepository;
-// Spring injects PatientRepository dependency through constructor Injection
-public PatientService(PatientRepository patientRepository)
+public class PatientService
 {
+ private PatientRepository patientRepository;
+ // Spring injects PatientRepository dependency through constructor Injection
+ public PatientService(PatientRepository patientRepository)
+ {
 
     this.patientRepository=patientRepository;
-}
-
-//NOTE: Service layer converts our domain entity model into Response DTO
+ }
+ //NOTE: Service layer converts our domain entity model into Response DTO
     //And the patient response DTO has the properties that we want the client to see.
-public List<PatientResponseDTO> getPatients()
-{
-
+ public List<PatientResponseDTO> getPatients()
+ {
     List<Patient> patients=patientRepository.findAll();
     List<PatientResponseDTO> patientResponseDTOS=
             patients.stream().map(patient -> PatientMapper.toDTO(patient))
                     .toList();
-
     return patientResponseDTOS;
-}
+ }
 }
