debaf3f8106a6aba708fcf24eeca40127cc50d0d
Abhishek Sharma <abhioptcs2000@gmail.com>
2026-05-23 21:37:29 +0530

    adding jwt auth into service layer

--- auth-service/src/main/java/com/pm/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pm/authservice/service/AuthService.java
@@ -3,6 +3,7 @@ package com.pm.authservice.service;
 import com.pm.authservice.dto.LoginRequestDTO;
 import com.pm.authservice.model.User;
 import com.pm.authservice.util.JwtUtil;
+import io.jsonwebtoken.JwtException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -41,6 +42,17 @@ public AuthService(UserService userService, PasswordEncoder passwordEncoder, Jwt
           .map(u->jwtUtil.generateToken(u.getEmail(),u.getRole()));
 
    return token;
+    }
 
+    public boolean validateToken(String token)
+    {
+        try{
+            jwtUtil.validateToken(token);
+            return true;
+        }
+        catch (JwtException e)
+        {
+            return false;
+        }
     }
 }
